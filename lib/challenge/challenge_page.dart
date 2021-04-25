3b0de0ab9301fcc78f9ba8f22e2c2efc883080f3
thhenrique <henriquethiago298@gmail.com>
2021-04-25 02:57:14 -0300

    fourth day of NLW 05

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,27 +1,97 @@
+import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
+import 'challenge_controller.dart';
+import 'widgets/next_button/next_button_widget.dart';
 import 'widgets/question_indicator/question_indicator_widget.dart';
 import 'widgets/quiz/quiz_widget.dart';
 
 class ChallengePage extends StatefulWidget {
-  ChallengePage({Key? key}) : super(key: key);
+  final List<QuestionModel> questions;
+  ChallengePage({Key? key, required this.questions}) : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
+
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+    super.initState();
+  }
+
+  void nextPage() {
+    if (controller.currentPage < widget.questions.length)
+      pageController.nextPage(
+        duration: Duration(milliseconds: 100),
+        curve: Curves.linear,
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: Size.fromHeight(86),
         child: SafeArea(
           top: true,
-          child: QuestionIndicatorWidget(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ValueListenableBuilder<int>(
+                valueListenable: controller.currentPageNotifier,
+                builder: (context, value, _) => QuestionIndicatorWidget(
+                  currentPage: value,
+                  length: widget.questions.length,
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: widget.questions.map((e) => QuizWidget(question: e, onChange: nextPage)).toList(),
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        minimum: EdgeInsets.only(bottom: 20),
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ValueListenableBuilder<int>(
+                valueListenable: controller.currentPageNotifier,
+                builder: (context, value, _) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        if (value < widget.questions.length)
+                          Expanded(
+                            child: NextButtonWidget.white(label: "Pular", onTap: nextPage),
+                          ),
+                        if (value == widget.questions.length) SizedBox(width: 7),
+                        if (value == widget.questions.length)
+                          Expanded(
+                            child: NextButtonWidget.green(
+                              label: "Finalizar",
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+                      ],
+                    ))),
+      ),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/challenge_page.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:devquiz/home/widgets/appBar/app_bar_widget.dart';
@@ -60,6 +61,12 @@ class _HomePageState extends State<HomePage> {
                             title: e.title,
                             percent: e.questionAnswered / e.questions.length,
                             completed: "${e.questionAnswered}/${e.questions.length}",
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => ChallengePage(questions: e.questions)),
+                              );
+                            },
                           ))
                       .toList(),
                 ),
